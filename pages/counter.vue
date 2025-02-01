f96725eed808032b8539716e8cf7aeecf0459d4c
kri_YamiHikari <kriYamiHikari@outlook.com>
2025-02-02 04:30:42 +0800

    feat: 完善计数器页面功能

--- pages/counter.vue
+++ pages/counter.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
-
+const num = ref(0)
+const increment = () => {
+  num.value++
+}
 </script>
 
 <template>
-  <div class="p-4">
-    我是计数器界面！
+  <div class="h-full flex justify-center items-center w-full">
+    <img class="w-full h-full object-cover" src="~/assets/images/64495434_p0.jpg" alt="bg">
+    <div class="bg-white bg-op-30 backdrop-blur-md rounded-md w-400px h-300px p-10 shadow-xl shadow-gray-5 shadow-op-50 fixed">
+      <div class="px-10 text-2xl font-bold mb-2 text-center text-white">Counter</div>
+      <div class="mb-10 text-white">
+        Count: {{num}}
+      </div>
+      <button class="rounded-md bg-white text-white bg-op-30 p-2 w-full hover:bg-op-60 hover:text-black transition-colors" @click="increment">+1</button>
+    </div>
   </div>
 </template>
 
