80b1fd4739776f9d12d08b0d600821a06b448e8e
kri_YamiHikari <kriYamiHikari@outlook.com>
2025-02-02 04:30:17 +0800

    refactor: 调整layout布局排版与大小

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,32 +10,35 @@ const menuTitle = ref("Nuxt练习项目")
 </script>
 
 <template>
-  <div class="flex h-screen w-full">
-
-    <aside class="h-full">
-      <div class="bg-white border-r border-gray-2 w-300px h-full">
-        <div class="flex flex-col p-4">
-          <div class="text-center py-2 font-bold text-xl">{{ menuTitle }}</div>
-          <!--suppress HtmlUnknownTarget -->
-          <NuxtLink v-for="item in route" :key="item.path" :to="item.path">
-            <button class="hover:bg-blue-2 rounded-md transition-colors border shadow-md shadow-gray-1 p-1 mb-2 w-full">
-              {{ item.name }}
-            </button>
-          </NuxtLink>
+  <div class="w-full h-screen">
+    <section class="flex flex-row h-full">
+      <aside class="h-full min-w-300px">
+        <div class="bg-white border-r border-gray-2 h-full">
+          <div class="flex flex-col p-4">
+            <div class="text-center py-2 font-bold text-xl">{{ menuTitle }}</div>
+            <!--suppress HtmlUnknownTarget -->
+            <NuxtLink v-for="item in route" :key="item.path" :to="item.path">
+              <button
+                  class="hover:bg-blue-2 rounded-md transition-colors border shadow-md shadow-gray-1 p-1 mb-2 w-full">
+                {{ item.name }}
+              </button>
+            </NuxtLink>
+          </div>
         </div>
-      </div>
-    </aside>
+      </aside>
 
-    <div class="h-full w-full bg-white flex flex-col">
-      <header class="w-full h-60px">
-        <div class="h-full flex items-center border-b border-gray-2 p-4 font-bold text-lg">
-          {{routeName}}
-        </div>
-      </header>
-      <main class="h-[calc(100vh-60px)] overflow-y-auto">
-        <NuxtPage/>
-      </main>
-    </div>
+      <section class="flex flex-col w-full h-full">
+        <header class="min-h-60px">
+          <div class="h-full flex items-center border-b border-gray-2 p-4 font-bold text-lg">
+            {{ routeName }}
+          </div>
+        </header>
+
+        <main class="h-full overflow-auto">
+          <NuxtPage/>
+        </main>
+      </section>
+    </section>
   </div>
 </template>
 
