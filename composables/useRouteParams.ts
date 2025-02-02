8d6b313394265e3358a48f4833c003f51fed6171
kri_YamiHikari <kriYamiHikari@outlook.com>
2025-02-03 06:34:07 +0800

    feat: 将布局使用Element ui组件重写，并支持侧边菜单图标显示。 - 添加空登陆界面占位，之后会完整实现。

--- composables/useRouteParams.ts
+++ composables/useRouteParams.ts
@@ -1,6 +1,7 @@
 export interface RouterItem {
     path: string,
-    name: string
+    name: string,
+    icon?: string
 }
 
 export const useNowRouteTitle = () => {
@@ -9,8 +10,9 @@ export const useNowRouteTitle = () => {
 
 export const useRouteDefine = () => {
     return useState<RouterItem[]>('routeDefine', () => [
-        {name: '首页', path: '/'},
-        {name: '计数器', path: '/counter'},
-        {name: '收藏列表', path: '/collections'}
+        {name: '首页', path: '/', icon: "HomeFilled"},
+        {name: '计数器', path: '/counter', icon: "Odometer"},
+        {name: '收藏列表', path: '/collections', icon: "Star"},
+        {name: '登陆', path: '/auth', icon: "Key"}
     ])
 }
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,43 +2,45 @@
 
 import {useNowRouteTitle, useRouteDefine} from "~/composables/useRouteParams";
 
-const route = useRouteDefine()
+const routePathList = useRouteDefine()
 const routeName = useNowRouteTitle()
+const route = useRoute()
 
 const menuTitle = ref("Nuxt练习项目")
 
 </script>
 
 <template>
-  <div class="w-full h-screen">
-    <section class="flex flex-row h-full">
-      <aside class="h-full min-w-300px">
+  <div>
+    <el-container class="h-screen">
+      <el-aside width="300px">
         <div class="bg-white border-r border-gray-2 h-full">
           <div class="flex flex-col p-4">
-            <div class="text-center py-2 font-bold text-xl">{{ menuTitle }}</div>
-            <!--suppress HtmlUnknownTarget -->
-            <NuxtLink v-for="item in route" :key="item.path" :to="item.path">
-              <button
-                  class="hover:bg-blue-2 rounded-md transition-colors border shadow-md shadow-gray-1 p-1 mb-2 w-full">
-                {{ item.name }}
-              </button>
-            </NuxtLink>
+            <div class="text-center py-4 font-bold text-xl">{{ menuTitle }}</div>
+            <el-menu class="border-none" :router="true" :default-active="route.path">
+              <el-menu-item class="rounded-md" v-for="item in routePathList" :key="item.path" :index="item.path">
+                <span v-if="item.icon !== undefined && item.icon !== ''">
+                  <el-icon>
+                    <Component :is="item.icon"/>
+                  </el-icon>
+                </span>
+                <span>{{ item.name }}</span>
+              </el-menu-item>
+            </el-menu>
           </div>
         </div>
-      </aside>
-
-      <section class="flex flex-col w-full h-full">
-        <header class="min-h-60px">
-          <div class="h-full flex items-center border-b border-gray-2 p-4 font-bold text-lg">
+      </el-aside>
+      <el-container>
+        <el-header class="p-0 border-b border-gray-2">
+          <div class="h-full flex items-center p-4 font-bold text-lg">
             {{ routeName }}
           </div>
-        </header>
-
-        <main class="h-full overflow-auto">
+        </el-header>
+        <el-main class="p-0">
           <NuxtPage/>
-        </main>
-      </section>
-    </section>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
